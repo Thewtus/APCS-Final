34a3baf878289924182b4bca21ecc404c2ffdd14
Cadyn <cadyn04@gmail.com>
2021-05-10 14:02:06 -0500

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -2,11 +2,13 @@ import java.awt.Image;
 
 public class Entity {
 
+	//basic stats
 	private int health;
 	private int attack;
 	private int defense;
 	private Image image;
 	
+	//constructor for basic stats
 	public Entity(int h, int a, int d, Image i)
 	{
 		health = h;
